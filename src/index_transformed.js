02232cae259cc0d24b589ed349d5e67cde111187
mak art <amin.khan@aacn.org>
2020-07-23 09:11:54 -0700

    ADD SCRIPTS ENV SERVER HTML

--- src/index_transformed.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var App = function App() {
-  return /*#__PURE__*/React.createElement("h1", null, "Hello from JSX!!");
-};
-
-var root = document.getElementById("root");
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: { index_transfomed: "./src/index.js" },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "src"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-react", "@babel/preset-env"] },
+        },
+      },
+    ],
+  },
+};
